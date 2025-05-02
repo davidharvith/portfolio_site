5e8b2542b88bfddb1285f62086baf260e8dea692
davidharvith <davidaviadh@gmail.com>
2025-05-02 14:51:25 -0400

    update CV

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Get absolute path to PDF
 const __dirname = path.resolve();
-const pdfPath = path.join(__dirname, 'David_Harvith_Resume.pdf');
+const pdfPath = path.join(__dirname, 'David_Harvith_CV.pdf');
 
 // Read PDF file on startup
 let resumeText = '';
