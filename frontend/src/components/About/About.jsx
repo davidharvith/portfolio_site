c2ef650132a7056871c277b55647b76cee7944be
davidharvith <davidaviadh@gmail.com>
2025-05-03 17:28:55 -0400

    removed .env

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -11,9 +11,27 @@ function About() {
       />
       <div className="about-text-box">
         <h2>Hi, I&apos;m David.</h2>
-        <p  >
+        <p>
           Welcome to my portfolio! I'm Aspiring softwear developer looking for my chance to join an incredibale team solving hard problems with a real impact on people's lives.
         </p>
+      </div>
+      <div className="about-buttons">
+        <a
+          href="https://www.linkedin.com/in/david-harvith-07302932b"
+          className="about-btn linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/davidharvith"
+          className="about-btn github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
       </div>
     </section>
   );
