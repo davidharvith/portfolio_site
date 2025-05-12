dfebb14db62a52df2eb934eb15ea242f0df67c3c
davidharvith <davidaviadh@gmail.com>
2025-05-12 08:35:09 -0400

    changed about

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -12,7 +12,7 @@ function About() {
       <div className="about-text-box">
         <h2>Hi, I&apos;m David.</h2>
         <p>
-          Welcome to my portfolio! I'm Aspiring softwear developer looking for my chance to join an incredibale team solving hard problems with a real impact on people's lives.
+          Welcome to my portfolio! I am A softwear developer looking for my chance to join an incredibale team solving hard problems with a real impact on people's lives.
         </p>
       </div>
       <div className="about-buttons">
